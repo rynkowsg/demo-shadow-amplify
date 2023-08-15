96c2bacac6d37977a9d6bcb9698f8c41cc633b23
Greg Rynkowski <5878299+rynkowsg@users.noreply.github.com>
2023-08-15 07:29:33 -0300

    Add init comments - missing http2
    ShadowCljs complains about
    ```
    The required JS dependency "http2" is not available, it was required by "node_modules/@aws-sdk/node-http-handler/dist/cjs/node-http2-handler.js".
    ```
    
    It shouldn't though because its node dependency.

---
+++ src/demo/config.cljs
@@ -0,0 +1,11 @@
+(ns demo.config)
+
+(def debug? ^boolean goog.DEBUG)
+
+(goog-define app-url "default-value")
+(goog-define aws-appsync-api-key "")
+(goog-define aws-appsync-graphql-endpoint "")
+(goog-define cognito-domain "")
+(goog-define identity-pool-id "")
+(goog-define user-pool-id "")
+(goog-define user-pool-web-client-id "")
